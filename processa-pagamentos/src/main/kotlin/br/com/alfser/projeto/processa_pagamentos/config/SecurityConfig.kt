fcfaf8d2eb2091c84cd1fce27a75d164b8e15e75
janilson.serra <j.janilson12@gmail.com>
2025-08-03 17:00:11 -0300

    fix: dasativar bloqueio do cors na API de processar pagamento

--- processa-pagamentos/src/main/kotlin/br/com/alfser/projeto/processa_pagamentos/config/SecurityConfig.kt
+++ processa-pagamentos/src/main/kotlin/br/com/alfser/projeto/processa_pagamentos/config/SecurityConfig.kt
@@ -6,17 +6,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class SecurityConfig {
-    @Bean
-    fun corsConfigurer(): WebMvcConfigurer {
-        return object : WebMvcConfigurer {
-            override fun addCorsMappings(registry: CorsRegistry) {
-                registry.addMapping("/**")
-                    .allowedOrigins("*") // Allow all origins
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                    .allowedHeaders("*")
-                    .maxAge(3600)
-            }
-        }
+class SecurityConfig: WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(false)
+            .maxAge(3600)
     }
 }
