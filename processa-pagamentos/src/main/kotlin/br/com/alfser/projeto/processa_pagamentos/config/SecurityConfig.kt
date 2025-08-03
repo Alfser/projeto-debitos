77f5d82d9f88f9cc51166b416b4278ecabee9659
janilson.serra <j.janilson12@gmail.com>
2025-08-03 00:43:17 -0300

    fix: ajuste no cors da api que processa pagamento

--- processa-pagamentos/src/main/kotlin/br/com/alfser/projeto/processa_pagamentos/config/SecurityConfig.kt
+++ processa-pagamentos/src/main/kotlin/br/com/alfser/projeto/processa_pagamentos/config/SecurityConfig.kt
@@ -1,4 +1,22 @@
 package br.com.alfser.projeto.processa_pagamentos.config
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
 class SecurityConfig {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("*") // Allow all origins
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .maxAge(3600)
+            }
+        }
+    }
 }
